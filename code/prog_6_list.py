d196a7b8e77108ac273737c70f4683dd228f9b92
Yogesh Garud <yogesh.p.garud@gmail.com>
2018-04-28 17:00:27 +0530

    update in the notes and prog-6 implemented

--- code/prog_6_list.py
+++ code/prog_6_list.py
@@ -1,9 +1,12 @@
+# define and access the list
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
 print(bicycles[0])
 print(bicycles[0].title())
 print "last element of the list is "
 print(bicycles[-1])
+
+# friends list creation and access test case
 names = ['pankaj', 'pushpendra', 'arihant', 'dhananjay', 'yugain']
 print names
 print names[0]
@@ -11,6 +14,8 @@ print names[1]
 print names[2]
 print names[3]
 print names[4]
+
+# adding a message in front of each friend name
 message = "Hi "
 print message + names[0]
 print message + names[1]
@@ -18,11 +23,46 @@ print message + names[2]
 print message + names[3]
 print message + names[4]
 print names
+
+# adding friend to list
 names.append("nitikesh")
 print names
+
+# add friend in location/order
 print "add test in the list at position 2 i.e python index 3"
 names.insert(3, "sushant")
 print names
+
+# remove a friend from list
 print "removed one friend from list !!!"
 del names[3]
 print names
+
+# remove friend name and store its value
+remove_item = names.pop(3)
+print remove_item
+print names
+
+# remove friend using value in the list
+names.remove('yugain')
+print names
+
+# sort the list
+names.sort()
+print names
+
+# sort the list in reverse order
+names.sort(reverse=True)
+print names
+
+# sort the list temporarily
+test_name = ["a", "A", "B", "b"]
+print sorted(test_name)
+
+
+# reverse the order of list
+test_name.reverse()
+print test_name
+
+# get length of the list
+print len(test_name)
